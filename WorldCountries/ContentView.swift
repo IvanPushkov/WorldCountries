daf77b1c9a388b35a01e82b6dd494e31aa07702a
IvanPushkov <Pushkov.ol@yandex.ru>
2025-01-28 11:30:55 +0300

    Initial Commit

---
+++ WorldCountries/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WorldCountries
+//
+//  Created by Ivan Pushkov on 28.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorldCountries/WorldCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldCountriesApp.swift
+//  WorldCountries
+//
+//  Created by Ivan Pushkov on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WorldCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
